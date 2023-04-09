999ed96bd27e94fe469c068eedc66dc99af54080
maricnikola <n.maric1912@gmail.com>
2023-04-09 17:24:50 +0200

    added base for doctor entity

--- ZdravoCorp/Core/User/Doctor.cs
+++ ZdravoCorp/Core/User/Doctor.cs
@@ -1,6 +1,16 @@
 namespace ZdravoCorp.Core.User;
 
-public class Doctor
+public class Doctor:User
 {
-    
+    public Specialization SpecialityType { get; set; }
+
+}
+
+public enum Specialization
+{
+    psychologist,
+    surgeon,
+    neurologist,
+    urologist,
+    anesthesiologist
 }
