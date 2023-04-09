865cabee797fee97a2d7e2241b8f4a9c37370659
c.danil0o <danilo.cvijetic10@protonmail.ch>
2023-04-09 11:29:08 -0700

    added login gui and binding

--- ZdravoCorp/View/LoginDialog.xaml.cs
+++ ZdravoCorp/View/LoginDialog.xaml.cs
@@ -1,19 +1,82 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 using ZdravoCorp.Core.Loader;
 
 namespace ZdravoCorp.View;
 
-public partial class LoginDialog : Window
+public partial class LoginDialog : Window, INotifyPropertyChanged
 {
+    private string? _email;
+    private string? _password;
+    public string Email
+    {
+        get
+        {
+            return _email;
+        }
+        set
+        {
+            if (value != _email)
+            {
+                _email = value;
+                OnPropertyChanged("EmailBox");
+            }
+        }
+    }
+
+    public string Password
+    {
+        get
+        {
+            return _password;
+        }
+        set
+        {
+            if (value != _password)
+            {
+                _password = value;
+                OnPropertyChanged("PasswordBox");
+            }
+        }
+    }
     public LoginDialog()
     {
         InitializeComponent();
+        DataContext = this;
     }
 
-    private void Button_OnClick(object sender, RoutedEventArgs e)
+    private void LoginButton_OnClick(object sender, RoutedEventArgs e)
     {
-        DialogResult = true;
-        //LoadFunctions.LoadUsers();
-        Close();
+        MessageBox.Show("email: " + Email + "   password: " + Password, "Test", MessageBoxButton.OK);
+        
+        
+        
+        /*DialogResult = true;
+        Close();*/
+    }
+
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+    {
+        Password = PasswordBox.Password;
     }
 }
