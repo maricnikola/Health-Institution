88089c2a84c5e8abbdd5e350c65e669005d62f2f
Veljko <ivanisevic.sv44.2021@uns.ac.rs>
2023-04-09 18:55:43 +0200

    Initial MedicalRecord commit

--- ZdravoCorp/Core/MedicalRecords/Repository/MedicalRecordRepository.cs
+++ ZdravoCorp/Core/MedicalRecords/Repository/MedicalRecordRepository.cs
@@ -1,6 +1,17 @@
+using System.Collections.Generic;
+using ZdravoCorp.Core.MedicalRecords.Model;
+
 namespace ZdravoCorp.Core.MedicalRecords.Repository;
 
 public class MedicalRecordRepository
 {
-    
+    private List<MedicalRecord> records { get; set; }
+    public void AddRecord(MedicalRecord newMedicalRecord)
+    {
+        records.Add(newMedicalRecord);
+    }
+    public void RemoveById(int id)
+    {
+
+    }
 }
