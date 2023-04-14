afae891bba0e9dfbe100665491097fdb365a5069
c.danil0o <danilo.cvijetic10@protonmail.ch>
2023-04-14 13:15:50 +0200

    added command base

---
+++ ZdravoCorp/Core/Commands/DirectorSearchCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace ZdravoCorp.Core.Commands;
+
+public class DirectorSearchCommand : CommandBase
+{
+    public override void Execute(object? parameter)
+    {
+        
+        //ovde kucate sta komanda radi
+        throw new System.NotImplementedException();
+    }
+}
