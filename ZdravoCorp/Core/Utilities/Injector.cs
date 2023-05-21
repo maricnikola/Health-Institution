275a9e0ca3926d45547bc14d637b74071f2663c8
c.danil0o <danilo.cvijetic10@protonmail.ch>
2023-05-21 19:06:19 +0200

    added Injector class #60

---
+++ ZdravoCorp/Core/Utilities/Injector.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Reflection;
+using Autofac;
+
+namespace ZdravoCorp.Core.Utilities;
+
+public static class Injector
+{
+    public static IContainer Container { get; set; }
+
+    public static void Configure()
+    {
+        var builder = new ContainerBuilder();
+
+        /*
+        builder.RegisterAssemblyTypes(Assembly.Load(nameof(ZdravoCorp)))
+            .Where(t => t.Namespace.Contains("Repositories"))
+            .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));*/
+        
+        Container = builder.Build();
+    }
+}
