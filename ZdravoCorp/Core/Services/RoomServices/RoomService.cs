1cfbca2c86f53b5d1e05bc7b2311faa50644bc34
c.danil0o <danilo.cvijetic10@protonmail.ch>
2023-06-13 16:36:51 +0200

    fixing multiple threads accessing same file for writing

--- ZdravoCorp/Core/Services/RoomServices/RoomService.cs
+++ ZdravoCorp/Core/Services/RoomServices/RoomService.cs
@@ -32,13 +32,15 @@ public class RoomService : IRoomService
     public void Update(int id, RoomDTO roomDto)
     {
         var oldRoom = _roomRepository.GetById(id);
-        if (oldRoom == null)
-        {
-            throw new KeyNotFoundException();
-        }
-        _roomRepository.Delete(oldRoom);
-        _roomRepository.Insert(new Room(roomDto));
-        DataChanged?.Invoke(this, new EventArgs());
+            if (oldRoom == null)
+            {
+                throw new KeyNotFoundException();
+            }
+            _roomRepository.Delete(oldRoom);
+            _roomRepository.Insert(new Room(roomDto));
+            DataChanged?.Invoke(this, new EventArgs());
+        
+
     }
 
     public bool UpdateRenovation(int id, bool status)
@@ -48,7 +50,7 @@ public class RoomService : IRoomService
         if (room == null)
             throw new KeyNotFoundException();
         if (room.IsUnderRenovation == status)
-            return false;
+            return true;
         var roomDto = new RoomDTO(room.Id, room.Type, status);
         Update(id, roomDto);
         DataChanged?.Invoke(this, EventArgs.Empty);
