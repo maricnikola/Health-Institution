5564d661915df2a947d699d511d2997514220c51
maricnikola <n.maric1912@gmail.com>
2023-06-13 19:17:15 +0200

    added view model to create annual leave request #51

---
+++ ZdravoCorp/Core/ViewModels/DoctorViewModels/DoctorAnnualLeaveViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ZdravoCorp.Core.Services.AnnualLeaveServices;
+
+namespace ZdravoCorp.Core.ViewModels.DoctorViewModels;
+
+public class DoctorAnnualLeaveViewModel : ViewModelBase
+{
+    public ICommand CreateAnnualLeaveCommand { get; private set; }
+	private IAnnualLeaveService _annualLeaveService;
+    public DoctorAnnualLeaveViewModel(IAnnualLeaveService annualLeaveService)
+    {
+		_annualLeaveService = annualLeaveService;
+
+    }
+	private DateTime _startDate;
+	public DateTime StartDate
+	{
+		get
+		{
+			return _startDate;
+		}
+		set
+		{
+			_startDate = value;
+			OnPropertyChanged(nameof(StartDate));
+		}
+	}
+	private DateTime _endDate;
+	public DateTime EndDate
+	{
+		get
+		{
+			return _endDate;
+		}
+		set
+		{
+			_endDate = value;
+			OnPropertyChanged(nameof(EndDate));
+		}
+	}
+
+	private string _reason;
+	public string Reason
+	{
+		get
+		{
+			return _reason;
+		}
+		set
+		{
+			_reason = value;
+			OnPropertyChanged(nameof(Reason));
+		}
+	}
+}
