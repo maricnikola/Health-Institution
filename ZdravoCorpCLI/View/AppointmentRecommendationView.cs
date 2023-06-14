061411052273d896da28885055699b09b43499df
aleksaqm <perovic.aleksa03@gmail.com>
2023-06-14 21:12:25 +0200

    made start and end time to be on 00,15,30,or 45 minutes

--- ZdravoCorpCLI/View/AppointmentRecommendationView.cs
+++ ZdravoCorpCLI/View/AppointmentRecommendationView.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Autofac;
 using ZdravoCorp.Core.HospitalSystem.Users.Models;
 using ZdravoCorp.Core.HospitalSystem.Users.Services;
+using ZdravoCorp.Core.Scheduling.Models;
 using ZdravoCorp.Core.Scheduling.Services;
 using ZdravoCorp.Core.Utilities;
 using ZdravoCorp.GUI.Scheduling.ViewModels;
@@ -78,7 +79,7 @@ public class AppointmentRecommendationView
     private void ChooseDoctor()
     {
         Console.WriteLine("Available Doctors:");
-        for (int i = 0; i < _doctorList.Count; i++)
+        for (var i = 0; i < _doctorList.Count; i++)
         {
             Console.WriteLine($"{i + 1}. {_doctorList[i]}");
         }
@@ -120,14 +121,19 @@ public class AppointmentRecommendationView
                     Time.WriteError("Invalid time. Please enter a valid time in the format HH:mm.");
                     continue;
                 }
+
                 if (i == 0)
+                {
                     _startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, time.Hours,
                         time.Minutes, 0);
+                    _startTime = TimeSlot.GiveFirstDevisibleBy15(_startTime);
+                }
                 else
                 {
                     _endTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, time.Hours,
                         time.Minutes, 0);
-                    if (_endTime < _startTime)
+                    _endTime = TimeSlot.GiveFirstDevisibleBy15(_endTime);
+                    if (_endTime <= _startTime)
                     {
                         Time.WriteError($"Invalid end time. Please enter a time that is afrer {_startTime}");
                         continue;
@@ -166,9 +172,10 @@ public class AppointmentRecommendationView
             var lastDate = new DateTime(_lastDate.Year, _lastDate.Month, _lastDate.Day, 23, 59, 0);
             Console.WriteLine("{0,-25} | {1, -25} | {2, -25}", "DATE", "DOCTOR", "PATIENT");
             Console.WriteLine(new string('-', 105));
+            List<Appointment> fittingAppointments = new List<Appointment>();
             if (_priority == "Doctor")
             {
-                var fittingAppointments =
+                fittingAppointments =
                     _scheduleService?.FindAppointmentsByDoctorPriority(_chosenDoctor, wantedTimeSlot, lastDate,
                         _patientEmail);
                 foreach (var appointmentForPrint in fittingAppointments)
@@ -178,7 +185,7 @@ public class AppointmentRecommendationView
             }
             else
             {
-                var fittingAppointments =
+                fittingAppointments =
                     _scheduleService?.FindAppointmentsByTimePriority(_chosenDoctor, wantedTimeSlot, lastDate,
                         _patientEmail, _doctorService);
                 foreach (var appointmentForPrint in fittingAppointments)
@@ -186,11 +193,51 @@ public class AppointmentRecommendationView
                     Console.WriteLine(new AppointmentViewModel(appointmentForPrint) + "\n");
                 }
             }
+            MakeAppointment(fittingAppointments);
+            Run();
         }
         catch (Exception e)
         {
             Console.WriteLine("An error occurred: " + e.Message);
         }
+    }
+
+    private void MakeAppointment(List<Appointment> appointments)
+    {
+        var appointmentIndex = GetAppointmentIndex(appointments);
+        if (appointmentIndex == -1)
+            return;
+        if (!_scheduleService.IsDoctorAvailable(appointments[appointmentIndex].Time,
+                appointments[appointmentIndex].Doctor.Email) ||
+            !_scheduleService.IsPatientAvailable(appointments[appointmentIndex].Time,
+                appointments[appointmentIndex].Doctor.Email)) return;
+
+        var appointmentDto = new AppointmentDTO(appointments[appointmentIndex].Id,
+            appointments[appointmentIndex].Time.Start, appointments[appointmentIndex].Doctor,
+            appointments[appointmentIndex].PatientEmail, appointments[appointmentIndex].Anamnesis);
+        _scheduleService.AddAppointment(appointmentDto);
+        Console.WriteLine("Appontment created successfully");
+
+    }
+    private static int GetAppointmentIndex(List<Appointment> appointments)
+    {
+        int appointmentIndex;
+        do
+        {
+            Console.Write("Enter the number of appointment you want to create (or x for exit): ");
+            var appointmentInput = Console.ReadLine();
+            if (appointmentInput.ToLower() == "x")
+            {
+                return -1;
+            }
+            if (!int.TryParse(appointmentInput, out appointmentIndex) || appointmentIndex < 1 ||
+                appointmentIndex > appointments.Count)
+            {
+                Console.WriteLine("Invalid input. Please try again.");
+            }
+        } while (appointmentIndex < 1 || appointmentIndex > appointments.Count);
+
+        return appointmentIndex - 1;
     }
 
 
