01c53fe6288b63f8fc96377ff74167c2f0f9e870
maricnikola <n.maric1912@gmail.com>
2023-06-14 02:34:09 +0200

    added a view for hospitalized patients and implemented the initial verison of the view model #53

---
+++ ZdravoCorp/Core/ViewModels/DoctorViewModels/HospitalRefferalViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZdravoCorp.Core.Models.HospitalRefferals;
+
+namespace ZdravoCorp.Core.ViewModels.DoctorViewModels;
+
+public class HospitalRefferalViewModel :ViewModelBase
+{
+    private HospitalRefferal _hospitalRefferal;
+    public string PatientMail => _hospitalRefferal.PatientMail;
+    public string StartTime => _hospitalRefferal.Time.Start.ToString();
+    public string EndTime => _hospitalRefferal.Time.End.ToString();
+
+    public HospitalRefferalViewModel(HospitalRefferal hospitalRefferal)
+    {
+        _hospitalRefferal = hospitalRefferal;
+    }
+}
---
+++ ZdravoCorp/Core/ViewModels/DoctorViewModels/HospitalizedPatientsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ZdravoCorp.Core.Models.HospitalRefferals;
+using ZdravoCorp.Core.Services.DoctorServices;
+using ZdravoCorp.Core.Services.HospitalRefferalServices;
+using ZdravoCorp.Core.Utilities;
+
+namespace ZdravoCorp.Core.ViewModels.DoctorViewModels;
+
+public class HospitalizedPatientsViewModel
+{
+    public ICommand ChangeTherapyCommand { get; private set; }
+    public ObservableCollection<HospitalRefferalViewModel> HospitalRefferals { get; }
+    private IHospitalRefferalService _hospitalRefferalService;
+    public HospitalRefferalViewModel SelectedHospitalRefferal { get; set; }
+    public  HospitalizedPatientsViewModel()
+    {
+        _hospitalRefferalService = Injector.Container.Resolve<IHospitalRefferalService>();
+        HospitalRefferals = new ObservableCollection<HospitalRefferalViewModel>();
+        foreach(HospitalRefferal hospitalRefferal in _hospitalRefferalService.GetAll())
+        {
+            HospitalRefferals.Add(new HospitalRefferalViewModel(hospitalRefferal));
+        }
+    }
+}
